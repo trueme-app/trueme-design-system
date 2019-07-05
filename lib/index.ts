d5dcee9e56ec273d85cdb0f74cea597ba66ea653
struct78 <david@struct.com.au>
2019-07-05 12:22:13 +1000

    Add missing border reference

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,11 @@
+import borders from './borders'
 import colours from './colours'
 import spacing from './spacing'
 import typeScale from './typeScale'
 import { setUnits, units } from './units'
 
 export {
+  borders,
   colours,
   setUnits,
   spacing,
